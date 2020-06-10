445a9523d6236bf2470d2d7fc6e2b9de265d1495
Jimmy Svensson <jimmy888swe@gmail.com>
2020-06-10 15:41:48 +0200

    fix: added dummy data

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -3,7 +3,10 @@ import React from 'react'
 import UsersList from '../components/UsersList';
 
 const Users = () => {
-  const USERS = [{id:'001', name:'Jimmy Svensson', image: 'https://en.wikipedia.org/wiki/StyleGAN#/media/File:Woman_1.jpg', places: 3}]
+  const USERS = [
+    {id:'001', name:'Jimmy Svensson', image: 'https://picsum.photos/200/300', placeCount: 3},
+    {id:'002', name:'Gabriella Mann', image: 'https://picsum.photos/200/300', placeCount: 2}
+  ]
   return <UsersList item={USERS} />
 }
 
