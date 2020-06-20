cc022d34b5c4be76b46437c0df570355a2a9d12f
Jimmy Svensson <jimmy888swe@gmail.com>
2020-06-20 23:14:39 +0200

    feat: first draft of navibar

---
+++ src/shared/components/Navigation/NavLinks.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './NavLinks.css'
+
+
+const  NavLinks = props => {
+  return (
+    <ul className="nav-links">
+    <li><NavLink to="/" exact>All USERS</NavLink></li>
+    <li><NavLink to="/001/places">MY PLACES</NavLink></li>
+    <li><NavLink to="/places/new">ADD PLACE</NavLink></li>
+    <li><NavLink to="/auth">AUTH</NavLink></li>
+    </ul>
+  )
+}
+
+export default NavLinks
