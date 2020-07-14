2293dec2cf79367a4086beb3c53248184107dd83
Jimmy Svensson <jimmy888swe@gmail.com>
2020-07-14 23:55:33 +0200

    fix: indentation

--- src/shared/components/FormElements/Button.js
+++ src/shared/components/FormElements/Button.js
@@ -31,9 +31,9 @@ const Button = props => {
     <button
       className={`button button--${props.size || 'default'} ${props.inverse &&
         'button--inverse'} ${props.danger && 'button--danger'}`}
-      type={props.type}
-      onClick={props.onClick}
-      disabled={props.disabled}
+        type={props.type}
+        onClick={props.onClick}
+        disabled={props.disabled}
     >
       {props.children}
     </button>
