14bb2cb1f02ff12837105ff108a4a742fa39203b
Jimmy Svensson <jimmy888swe@gmail.com>
2020-12-05 16:49:13 +0100

    fix: typo

--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -25,9 +25,9 @@ const inputReducer = (state, action) => {
 
 const Input = props => {
   const [inputState, dispatch] = useReducer(inputReducer, {
-    value: props.initalValue || '',
+    value: props.initialValue || '',
     isTouched: false,
-    isValid: props.initalValid || false
+    isValid: props.initialValid || false
   });
   
   const { id, onInput } = props;
