053761485f4b710a3417d5c4e631f61de3684467
Jimmy Svensson <jimmy888swe@gmail.com>
2020-12-05 22:59:57 +0100

    fix: new to:

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -8,7 +8,7 @@ const  NavLinks = props => {
   return (
     <ul className="nav-links">
     <li><NavLink to="/" exact>All USERS</NavLink></li>
-    <li><NavLink to="/001/places">MY PLACES</NavLink></li>
+    <li><NavLink to="/u1/places">MY PLACES</NavLink></li>
     <li><NavLink to="/places/new">ADD PLACE</NavLink></li>
     <li><NavLink to="/auth">AUTH</NavLink></li>
     </ul>
