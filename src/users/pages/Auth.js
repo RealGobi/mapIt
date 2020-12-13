48d5607d774e7497a11c93369bd49f9079b08860
Jimmy Svensson <jimmy888swe@gmail.com>
2020-12-13 15:56:07 +0100

    feat: loginhandler

--- src/users/pages/Auth.js
+++ src/users/pages/Auth.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react'
 import Input from '../../shared/components/FormElements/Input';
 import './Auth.css';
 const Auth = () => {
+  const loginHandler = e => {
+    e.preventDefault();
+    console.log(formState.inputs);
+  };
   return (
     <div>
     </div>
