db85e37148a45dec6de4179dfa76c8cf28541763
Jimmy Svensson <jimmy888swe@gmail.com>
2020-12-13 15:55:38 +0100

    feat: draft of auth comp

---
+++ src/users/pages/Auth.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import Input from '../../shared/components/FormElements/Input';
+import './Auth.css';
+const Auth = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default Auth
